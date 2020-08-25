7736f21930c7c9edbfd1e0419eda4c1f73baf11f
Saidur Rahman Sajol <rahmansaidur970@gmail.com>
2020-08-26 05:19:29 +0600

    Create app.js

---
+++ JS/app.js
@@ -0,0 +1,231 @@
+var count=3;
+var tot=0;
+
+function b1(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b2(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b3(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b4(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b5(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b6(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b7(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b8(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b9(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b10(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b11(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b12(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b13(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b14(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b15(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b16(){
+    if(count>0){
+    var rand=Math.floor((Math.random()*10)+1);
+    if(rand%2!=0) {
+        ++tot;
+        alert("Your point has been increased by +1");
+    }
+    else{
+        --count;
+        if(count>1) alert("You have "+count+" lives remaining!");
+        else if(count==1) alert("You have "+count+" life remaining!");
+        else document.getElementById("dd").innerHTML=tot;
+    }}
+}
+function b17(){
+    location.reload();
+    return false;
+}
